f96bf5ac7aef8693e715413cfbe6cb8bbbce9726
Anna Lena Baumann <rescht@posteo.de>
2022-03-10 17:25:55 +0100

    mongodb atlas use

--- src/test/java/de/codexbella/week6quiz/Week6QuizApplicationTests.java
+++ src/test/java/de/codexbella/week6quiz/Week6QuizApplicationTests.java
@@ -1,15 +1,27 @@
 package de.codexbella.week6quiz;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Week6QuizApplicationTests {
+   @Autowired
+   private TestRestTemplate restTemplate;
 
-    @Test
-    @Disabled
-    void contextLoads() {
-    }
+   @Test
+   void contextLoads() {
+      Question question = new Question();
+      question.setQuestion("Warum ist der Himmel blau?");
 
+      ResponseEntity<Question> response = restTemplate.postForEntity("/", "Warum ist der Himmel blau?", Question.class);
+      Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+      Assertions.assertThat(response.getBody().getId()).isNotNull();
+      Assertions.assertThat(response.getBody().getQuestion()).isEqualTo("Warum ist der Himmel blau?");
+   }
 }
